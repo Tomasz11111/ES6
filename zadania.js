83add0d31e7e8578aaa6f3e0733811a028892498
Tomasz11111 <tomasz.dziendziura@gmail.com>
2017-12-30 13:50:36 +0100

    zadanie poprawione

--- zadania.js
+++ zadania.js
@@ -2,54 +2,38 @@
 
 let hello = 'Hello';
 let world = 'World';
-let newString = `${hello} ${world}`;
-console.log(newString);
+
+console.log(`${hello} ${world}`);
 
 //zadanie 2
 
-const multiply = (x, y = 1) => x * y;
+const multiply =(x, y = 1) => x * y;
 console.log(multiply(2)) // zwraca 2, czyli 2 * 1
 console.log(multiply(2, 5)); //zwraca 10
 
 //zadanie 3
 
-function average(...numbers) {
+const average = (...numbers) => numbers.reduce((prev, current) => prev + current)/numbers.length;
 	
-	return (numbers.reduce((prev, current) => prev + current))/numbers.length
-	
-	
-}
 console.log(average(2,4,8,5)) //zwróci średnią, czyli 4.75
 console.log(average(5)) //zwróci 5
 console.log(average(100, 1000)) // zwróci 550
 
 //zadanie 4
 
-function average2(x, y) {
-	
-	return (grades.reduce((prev, current) => prev + current))/grades.length
-	
-	
-}
+
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
-console.log(average2(...grades)) //zwróci średnią, czyli 3.22222222222
-
-
-var average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
-
-average( grades );
+console.log(average(...grades)) //wyświetlni średnią, czyli 3.22222222222
 
 
 // Zadanie 5
 
 
 const names = [1,4, 'Iwona', false, 'Nowak'];
-const [ , , third, , fifth] = names
+const [ , , firstName , ,lastName] = names
 
-const firstName = third;
-const lastName = fifth;
 console.log(firstName); //wyświetli Iwona
-console.log(fifth) // wyświetli Nowak
+console.log(lastName) // wyświetli Nowak
 
 
 
